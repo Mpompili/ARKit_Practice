fd63a71fb3d4bca421ede05ba9eff336ddaf6e73
Michael Pompili <michael@churroboros.com>
2018-06-20 14:47:00 -0700

    practice with shapes

--- world tracking/world tracking/ViewController.swift
+++ world tracking/world tracking/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
-        
+//
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,14 +31,21 @@ class ViewController: UIViewController {
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.darkGray
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        node.geometry = shape
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         
-        let x = randoNumbers(firstNum: -0.3, secondNum: 0.3)
-        let y = randoNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let x = randoNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let y = randoNumbers(firstNum: -0.3, secondNum: 0.3)
         
-        node.position = SCNVector3(x, y, -0.3)
+        node.position = SCNVector3(0, 0, -0.6)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     @IBAction func reset(_ sender: Any) {
