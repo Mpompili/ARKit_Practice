0ec120be0cee06901ccaf1282a089028153f91bb
Michael Pompili <michael@churroboros.com>
2018-06-24 13:11:11 -0700

    adding external 3d models

--- ARGame/ARGame/ViewController.swift
+++ ARGame/ARGame/ViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var sceneView: ARSCNView!
+    let config = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(config)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    func addNode() {
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0, 0, -1.5)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("didn't hit anything")
+        } else {
+           print("touched obj")
+        }
     }
 
 
