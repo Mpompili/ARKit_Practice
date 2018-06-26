cf5c463cf6d6114c8896468aea4b030750187c72
Michael Pompili <michael@churroboros.com>
2018-06-26 14:20:16 -0700

    floor is lava

--- PlaneDetection/PlaneDetection/ViewController.swift
+++ PlaneDetection/PlaneDetection/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+//        let lava =  createLava(planeAnchor: <#T##ARPlaneAnchor#>)
+//        self.sceneView.scene.rootNode.addChildNode(lava)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = #imageLiteral(resourceName: "lava")
+        material.diffuse.contentsTransform = SCNMatrix4MakeScale(6, 6, 0)
+        lavaNode.geometry?.firstMaterial? = material
+        lavaNode.geometry?.firstMaterial?.diffuse.wrapS = SCNWrapMode.repeat
+        lavaNode.geometry?.firstMaterial?.diffuse.wrapT = SCNWrapMode.repeat
+        
+//        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
+        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        lavaNode.eulerAngles = SCNVector3(-90.degreesToRadians, 0, 0 )
+        return lavaNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        print("new flat surface detected")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("planeAnchor updated")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let _ = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        print("removed planeAnchor")
+    }
 
+}
 
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180 }
 }
 
